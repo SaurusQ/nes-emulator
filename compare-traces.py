f59895f9b55bb5dbcc988d211c8a0225348dae47
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2025-10-25 15:19:18 +0300

    improve python trace parser

--- compare-traces.py
+++ compare-traces.py
@@ -1,22 +1,23 @@
-# compare_traces.py
+import argparse
 
 truth_path = "./program/nestest.log"
 trace_path = "./trace/trace.log"
 
-split_lines_trace = 2
+skip_trace_lines_start = 2
 
 RED = "\033[31m"
 RESET = "\033[0m"
 
-def compare_traces(truth_path, trace_path):
+def compare_traces(truth_path, trace_path, skip_lines):
 
     truth = open(truth_path, "r")
     trace = open(trace_path, "r")
 
-    for _ in range(split_lines_trace):
+    for _ in range(skip_trace_lines_start):
         trace.readline()
 
     line_num = 1
+    first_diff = False
     while True:
         truth_line = truth.readline()
         trace_line = trace.readline()
@@ -41,10 +42,31 @@ def compare_traces(truth_path, trace_path):
                 line_display += truth_line[i]
                 continue
 
+            first_diff = True
             line_display += f"{RED}{trace_line[i]}{RESET}"
 
-        print(f"{line_num:3}: {line_display}")
+        if first_diff or not skip_lines:
+            print(f"{line_num:3}: {line_display}")
         line_num += 1
 
 if __name__ == "__main__":
-    compare_traces(truth_path, trace_path)
+    parser = argparse.ArgumentParser(description="Compare two NES .log trace files")
+    parser.add_argument(
+        "truth",
+        nargs="?",
+        default=truth_path,
+        help="Path to original .log file",
+    )
+    parser.add_argument(
+        "trace",
+        nargs="?",
+        default=trace_path,
+        help="Path to trace file",
+    )
+    parser.add_argument(
+        "-s", "--skip-lines", action="store_true",
+        help="Skip lines before first diff"
+    )
+
+    args = parser.parse_args()
+    compare_traces(args.truth, args.trace, args.skip_lines)
